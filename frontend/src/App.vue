471c6d4dfdf504a8765a23474a634d79f6c7b0b3
Andrei Zhulid <>
2022-11-28 12:38:49 +0100

    Added SearchView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      
+      <SearchView/>
     </v-main>
   </v-app>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SearchView from '@/views/SearchView.vue';
+
+@Component({ components: { SearchView } })
+export default class App extends Vue {}
+</script>
