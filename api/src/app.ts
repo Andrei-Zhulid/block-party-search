8d66489b944f0f3d1acc1ca35e3ead04512f3a9d
Andrei Zhulid <>
2022-11-28 13:09:11 +0100

    Changed endpoint

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,12 +7,12 @@ import SearchService from './services/SearchService';
 
 const app: Application = express();
 
-app.get('/search', async (request: Request, response: Response) => {
+app.get('/api/v1/search', async (request: Request, response: Response) => {
   try {
     const places = await SearchService.search(<string>request.query.query);
     response.send(places);
   } catch (error) {
-    console.log('GET /search error', error);
+    console.log('GET /api/v1/search error', error);
     response.status(500).end();
   }
 })
