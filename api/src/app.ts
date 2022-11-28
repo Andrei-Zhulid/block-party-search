1127750976a1a5882ed025a80a90867b9eda7901
Andrei Zhulid <>
2022-11-28 10:10:27 +0100

    Implemented '/search' endpoint

---
+++ api/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+app.get('/search', (request: Request, response: Response) => {
+  try {
+    response.status(200).end();
+  } catch (error) {
+    console.log('GET /search error', error);
+    response.status(500).end();
+  }
+})
+
+app.listen(process.env.PORT, () => console.log('Service is running'));
