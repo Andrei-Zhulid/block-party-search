dbd0364f499e13ed9175e4030fbac0c90b1c9b71
Andrei Zhulid <>
2022-11-28 14:42:15 +0100

    Display all parties when SearchView is opend

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <SearchInput/>
+    <SearchInput class="mb-6"/>
 
-    <v-container>
+    <v-container class="pa-0">
+      <v-row v-show="!loading && items?.length > 0">
+        <SearchResultsColumn
+          v-for="item in items"
+          :key="item.id"
+        >
+          <BlockPartyCard :place="item"/>
+        </SearchResultsColumn>
+      </v-row>
       <SearchResultsRowLoader v-show="loading"/>
     </v-container>
   </div>
@@ -13,9 +21,28 @@ import { Component, Vue } from 'vue-property-decorator';
 import SearchInput from '@/components/SearchInput.vue';
 import SearchResultsColumn from '@/components/SearchResultsColumn.vue';
 import SearchResultsRowLoader from '@/components/SearchResultsRowLoader.vue';
+import IBlockParty from '../../../common/interfaces/IBlockParty';
+import SearchService from '@/services/SearchService';
+import BlockPartyCard from '@/components/BlockPartyCard.vue';
 
-@Component({ components: { SearchResultsRowLoader, SearchResultsColumn, SearchInput } })
+@Component({ components: { BlockPartyCard, SearchResultsRowLoader, SearchResultsColumn, SearchInput } })
 export default class HomeView extends Vue {
+  private items: IBlockParty[] = [];
   private loading = false;
+
+  private async search(query?: string) {
+    try {
+      this.loading = true;
+      this.items = await SearchService.search(query);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async created() {
+    await this.search();
+  }
 }
 </script>
